73bc7174a79a7837e5963e8c702aaf3b73caa343
Ezar Prasetya <prasetyaezar@gmail.com>
2024-11-10 23:52:30 +0700

    Menambahkan program untuk konek ke Firebase

---
+++ UI/UX Designer/v2/scripts/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3MtP1x7CisxQr4ZJsyff0fkD2wVIu_b8",
+    authDomain: "griyatech-78ff8.firebaseapp.com",
+    databaseURL: "https://griyatech-78ff8-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "griyatech-78ff8",
+    storageBucket: "griyatech-78ff8.appspot.com",
+    messagingSenderId: "363118645444",
+    appId: "1:363118645444:web:7fbbd991f8fdebd4d33991",
+    measurementId: "G-4ZRHMHVH7X"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
